9833f385dab6c40a69484960958c33602073f2e4
ibizugbe <abrahamibizugbe@gmail.com>
2022-06-27 17:53:43 +0100

    remove readme file

--- index.js
+++ index.js
@@ -1,37 +1,37 @@
-const inputAuthor = document.querySelector(".input-books-author");
-const inputTitle = document.querySelector(".input-books-title");
-const bookContainer = document.querySelector(".awesome-books");
-const addBook = document.querySelector(".add-books");
+const inputAuthor = document.querySelector('.input-books-author');
+const inputTitle = document.querySelector('.input-books-title');
+const bookContainer = document.querySelector('.awesome-books');
+const addBook = document.querySelector('.add-books');
 
 // empty library array for storing book as object
-const library = JSON.parse(localStorage.getItem("books") || "[]");
+const library = JSON.parse(localStorage.getItem('books') || '[]');
 
 // existing entries
-const existingEntries = JSON.parse(localStorage.getItem("books") || "[]");
+const existingEntries = JSON.parse(localStorage.getItem('books') || '[]');
 
 // Update User Interface dynamically
 
 const updateUI = () => {
-  bookContainer.innerHTML = "";
+  bookContainer.innerHTML = '';
   library.forEach((data, i) => {
-    const bookSelf = document.createElement("div");
-    bookSelf.classList.add("bookItem");
-    const newBookTitle = document.createElement("p");
+    const bookSelf = document.createElement('div');
+    bookSelf.classList.add('bookItem');
+    const newBookTitle = document.createElement('p');
     newBookTitle.textContent = `${data.title}`;
-    const newBookAuthor = document.createElement("p");
+    const newBookAuthor = document.createElement('p');
     newBookAuthor.textContent = `${data.author}`;
-    const button = document.createElement("button");
-    button.textContent = "Remove";
-    const separator = document.createElement("hr");
+    const button = document.createElement('button');
+    button.textContent = 'Remove';
+    const separator = document.createElement('hr');
 
     // remove books from library
     const removeBook = function () {
       library.splice(this, 1);
-      localStorage.setItem("books", JSON.stringify(library));
+      localStorage.setItem('books', JSON.stringify(library));
       updateUI(library);
     };
 
-    button.addEventListener("click", removeBook.bind(i));
+    button.addEventListener('click', removeBook.bind(i));
 
     bookSelf.appendChild(newBookTitle);
     bookSelf.appendChild(newBookAuthor);
@@ -45,16 +45,16 @@ const updateUI = () => {
 updateUI();
 // add new books to library
 
-addBook.addEventListener("click", (e) => {
+addBook.addEventListener('click', (e) => {
   e.preventDefault();
 
   if (inputAuthor.value && inputTitle.value) {
     const book = { author: inputAuthor.value, title: inputTitle.value };
     library.push(book);
     updateUI(library);
-    inputAuthor.value = "";
-    inputTitle.value = "";
+    inputAuthor.value = '';
+    inputTitle.value = '';
     existingEntries.push(book);
-    localStorage.setItem("books", JSON.stringify(existingEntries));
+    localStorage.setItem('books', JSON.stringify(existingEntries));
   }
 });
