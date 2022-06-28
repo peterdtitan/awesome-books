a5e417edfdea3663f2b8bea935da559657c9669a
Peter D Titan <peterdtitan@Peters-MacBook-Pro.local>
2022-06-28 07:46:36 +0300

    pending

--- modules/local_storage.js
+++ modules/local_storage.js
@@ -7,12 +7,12 @@ export default function storageAvailable(type) {
     storage.setItem(x, x);
     storage.removeItem(x);
     return true;
-  } catch (e) {
-    return e instanceof DOMException && (
-        e.code === 22
-        || e.code === 1014
-        || e.name === 'QuotaExceededError'
-        || e.name ===  'NS_ERROR_DOM_QUOTA_REACHED')
-        && storage.length !== 0;
+  } catch (e) {   return e instanceof DOMException && (
+    e.code === 22
+    || e.code === 1014
+    || e.name === 'QuotaExceededError'
+    || e.name ===  'NS_ERROR_DOM_QUOTA_REACHED')
+    && storage.length !== 0;
   }
 }
+
