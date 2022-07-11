793f3bf95ab1672437efad5d724341c5b85999d1
Peter D Titan <peterdtitan@Peters-MacBook-Pro.local>
2022-07-11 15:16:28 +0300

    Make changes to readme and update arrow functions

--- modules/local_storage.js
+++ modules/local_storage.js
@@ -1,18 +1,13 @@
-export default function storageAvailable(type) {
-  // Check if Storage is available
-  let storage;
+const storageAvailable = (type) => {
   try {
-    storage = window[type];
-    const x = '__storage_test__';
+    var storage = window[type],
+      x = '__storage_test__';
     storage.setItem(x, x);
     storage.removeItem(x);
     return true;
   } catch (e) {
-    return e instanceof DOMException && (
-      e.code === 22
-        || e.code === 1014
-        || e.name === 'QuotaExceededError'
-        || e.name === 'NS_ERROR_DOM_QUOTA_REACHED')
-      && (storage && storage.length !== 0);
+    return false;
   }
 }
+
+export default storageAvailable;
